f34112624a0a29b8e5790cb01a19f53ab7028f06
rohithmsr <rohit.rajaram@arcadia.com>
2023-07-02 15:39:29 +0530

    add validation

--- app/models/tour.rb
+++ app/models/tour.rb
@@ -1,2 +1,4 @@
 class Tour < ApplicationRecord
+  validates :name, presence: true
+  validates :rating, presence: true, :numericality => { :greater_than_or_equal_to => 0, :less_than_or_equal_to => 5, message: 'must be from 0 to 5' }
 end
