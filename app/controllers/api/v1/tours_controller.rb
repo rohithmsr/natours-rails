fbd4ea76884629484ee2a2f64bfae855dfa33017
rohithmsr <rohit.rajaram@arcadia.com>
2023-07-14 22:48:38 +0530

    fix: api_v1_url errors and correct status codes

--- app/controllers/api/v1/tours_controller.rb
+++ app/controllers/api/v1/tours_controller.rb
@@ -1,6 +1,8 @@
 module Api
   module V1
     class ToursController < Api::V1::BaseController
+      rescue_from ActiveRecord::RecordNotFound, with: :not_found
+
       def index
         query = Api::Queries::Tours.new(index_params)
         tours = query.records
@@ -9,47 +11,51 @@ module Api
       end
 
       def show
-        @tour = Tour.find(params[:id])
-        render json: @tour
+        tour = Tour.find(params[:id])
+        render json: tour
       end
 
       def create
-        @tour = Tour.new(tour_params)
-        if @tour.save
-          redirect_to @tour
+        tour = Tour.new(tour_params)
+        if tour.save
+          render json: tour, status: :created
         else
-          render json: { errors: @tour.errors.full_messages }
+          render json: { errors: tour.errors.full_messages }, status: :unprocessable_entity
         end
       end
 
       def update
-        @tour = Tour.find(params[:id])
-        if @tour.update(tour_params)
-          redirect_to @tour
+        tour = Tour.find(params[:id])
+        if tour.update(tour_params)
+          render json: { tour: tour }
         else
-          render json: { errors: @tour.errors.full_messages }
+          render json: { errors: tour.errors.full_messages }, status: :unprocessable_entity
         end
       end
 
       def destroy
-        @tour = Tour.find(params[:id])
-        @tour.destroy
+        tour = Tour.find(params[:id])
+        tour.destroy!
 
-        redirect_to tours_path, notice: "Tour #{params[:id]} deleted"
+        head :no_content
       end
 
       private
 
-      def tour_params
-        params.require(:tour).permit(:name, :key, :rating, :duration, :difficulty, :price, :price_discount, :description)
-      end
+        def tour_params
+          params.require(:tour).permit(:name, :key, :rating, :duration, :difficulty, :price, :price_discount, :description)
+        end
 
-      def index_params
-        params.permit(
-          :page,
-          :limit
-        )
-      end
+        def index_params
+          params.permit(
+            :page,
+            :limit
+          )
+        end
+
+        def not_found(message)
+          render json: { error: message }, status: :not_found
+        end
     end
   end
 end
