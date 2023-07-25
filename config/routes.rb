1c0f675a3614e587453e6850e7ae1d829608f6aa
rohithmsr <rohit.rajaram@arcadia.com>
2023-07-25 17:35:26 +0530

    Traveller Model

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :travellers
   namespace :api, constraints: { format: :json } do
     namespace :v1 do
       resources :tours
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_07_09_041944) do
+ActiveRecord::Schema.define(version: 2023_07_25_120142) do
 
   create_table "tours", force: :cascade do |t|
     t.string "name"
@@ -24,6 +24,18 @@ ActiveRecord::Schema.define(version: 2023_07_09_041944) do
     t.text "description"
     t.string "key"
     t.index ["key"], name: "index_tours_on_key", unique: true
+  end
+
+  create_table "travellers", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["email"], name: "index_travellers_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_travellers_on_reset_password_token", unique: true
   end
 
 end
